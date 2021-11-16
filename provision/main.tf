e1d674dfb203cc5de69610c156cf3255204cd67d
Samuil Plamenov <samuil.plamenov@harpercollins.co.uk>
2021-11-16 14:52:14 +0000

    Migrated the database to Terraform

--- provision/main.tf
+++ provision/main.tf
@@ -35,13 +35,42 @@ resource "azurerm_app_service" "main" {
   app_service_plan_id = azurerm_app_service_plan.main.id
 
   site_config {
-    app_command_line  = ""
+    app_command_line = ""
     linux_fx_version = "DOCKER|corndelldevopscourse/mod12app:latest"
   }
 
   app_settings = {
     "SCM_DO_BUILD_DURING_DEPLOYMENT" : "True"
     "DEPLOYMENT_METHOD" : "Terraform",
-    "CONNECTION_STRING" = "Server=tcp:samuilpetrov-non-iac-sqlserver.database.windows.net,1433;Initial Catalog=samuilpetrov-non-iac-db;Persist Security Info=False;User ID=db;Password=${var.database_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+    "CONNECTION_STRING" = "Server=tcp:${azurerm_sql_server.main.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_sql_database.main.name};Persist Security Info=False;User ID=${azurerm_sql_server.main.administrator_login};Password=${var.database_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+  }
+}
+
+resource "azurerm_sql_server" "main" {
+  name                         = "samuilpetrov-non-iac-sqlserver"
+  resource_group_name          = data.azurerm_resource_group.main.name
+  location                     = data.azurerm_resource_group.main.location
+  version                      = "12.0"
+  administrator_login          = "db"
+  administrator_login_password = var.database_password
+
+  tags = {
+    environment = "production"
+  }
+}
+
+resource "azurerm_sql_database" "main" {
+  name                = "samuilpetrov-non-iac-db"
+  resource_group_name = data.azurerm_resource_group.main.name
+  location            = data.azurerm_resource_group.main.location
+  server_name         = azurerm_sql_server.main.name
+  edition             = "Basic"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = {
+    environment = "production"
   }
 }
